e22a7876fcc71be9ad99875957fb05e0a8172c6d
Documenter.jl <documenter@juliadocs.github.io>
2023-11-07 15:34:54 +0000

    build based on fc99a7e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = UnfoldStats","category":"page"},{"location":"#UnfoldStats","page":"Home","title":"UnfoldStats","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for UnfoldStats.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [UnfoldStats]","category":"page"}]
+}
