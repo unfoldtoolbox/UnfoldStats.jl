db14b9d1737f0a4a9250999a159dbb3e2a4a564d
jschepers <judith.schepers@vis.uni-stuttgart.de>
2024-03-11 15:19:59 +0000

    adapt test infrastructure and add first version of tests for extract_coefs

--- benchmark/sim_and_fit.jl
+++ benchmark/sim_and_fit.jl
@@ -270,28 +270,3 @@ function create_component(::MultiChannel, basis, formula, β, σs)
         MultichannelComponent(signal, projection[channels, source_idx], NoNoise())
     return multichannel_signal
 end
-
-abstract type SimulationType end
-struct UnitTestSimulation <: SimulationType end
-struct BenchmarkSimulation <: SimulationType end
-
-abstract type PredictorStyle end
-struct OnlySplines <: PredictorStyle end
-struct ManyPredictors <: PredictorStyle end
-
-abstract type ChannelStyle end
-struct SingleChannel <: ChannelStyle end
-struct MultiChannel <: ChannelStyle end
-
-abstract type EventStyle end
-struct SingleEventType <: EventStyle end
-struct MultipleEventTypes <: EventStyle end
-
-PredictorStyle(::Type) = OnlySplines()
-PredictorStyle(::Type{UnitTestSimulation}) = ManyPredictors()
-
-ChannelStyle(::Type) = SingleChannel()
-ChannelStyle(::Type{UnitTestSimulation}) = MultiChannel()
-
-EventStyle(::Type) = SingleEventType()
-EventStyle(::Type{UnitTestSimulation}) = MultipleEventTypes()
--- test/setup.jl
+++ test/setup.jl
@@ -2,5 +2,7 @@ using Test
 using UnfoldSim
 using Unfold
 using DataFrames
+using StableRNGs
 
+include("../benchmark/types.jl")
 include("../benchmark/sim_and_fit.jl")
