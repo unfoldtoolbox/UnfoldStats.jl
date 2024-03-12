0777148d3b65e13d5194c2162e0bad1c98e8e2da
jschepers <judith.schepers@vis.uni-stuttgart.de>
2024-03-12 14:05:26 +0000

    added docstrings for extract_coefs function and its methods

--- src/extract_coefs.jl
+++ src/extract_coefs.jl
@@ -38,7 +38,21 @@ end
 get_basisnames(model::UnfoldLinearModelContinuousTime) =
     Unfold.extract_coef_info(Unfold.get_coefnames(model), 1)
 
+"""
+    extract_coefs(model::UnfoldModel, predictor, basisname)
 
+Return the coefficients of an Unfold model for a certain `predictor` and `basisname`.
+
+For extracting the terms of a predictor variable `predictor` must be a symbol e.g. :continuous.
+For extracting the intercept `predictor` should be a String, i.e. "(Intercept)".
+
+`basisname` must match one of the basis names which can be found in `coeftable(model)`.
+
+Note: If a predictor variable has more than one term in the formula (e.g. a spline set, a categorical variable with several levels or an interaction),
+the coefficients for all terms are returned.
+
+The dimensions of the returned coefficients are channel x times x coefs.
+"""
 function extract_coefs(model::UnfoldModel, predictor, basisname)
 
     # Get vector with underlying predictor variable (symbol) for all coefficients
@@ -116,6 +130,13 @@ extract_coefs(
     "The `extract_coefs` function is currently not implemented for mixed-effects models.",
 )
 
+"""
+    extract_coefs(models::Vector{<:UnfoldModel}, predictor, basisname)
+
+When applied to a vector of Unfold models, extracts the coefficients (matching the predictor and basisname) for all models (usually subjects) and concatenates them.
+
+The dimensions of the returned coefficients are channel x times x coefs x subjects.
+"""
 function extract_coefs(models::Vector{<:UnfoldModel}, predictor, basisname)
 
     # Extract the coefficients for all subjects
