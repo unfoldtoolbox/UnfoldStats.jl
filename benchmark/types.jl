03f851f921760ef34ba1b50fa3fd065ff171b194
jschepers <judith.schepers@vis.uni-stuttgart.de>
2024-03-18 11:07:31 +0000

    reordered simulation types

--- benchmark/types.jl
+++ benchmark/types.jl
@@ -1,24 +1,28 @@
 abstract type SimulationType end
-struct UnitTestSimulation <: SimulationType end
-struct BenchmarkSimulation <: SimulationType end
 
 abstract type PredictorStyle end
 struct OnlySplines <: PredictorStyle end
 struct ManyPredictors <: PredictorStyle end
+PredictorStyle(::Type) = OnlySplines()
 
 abstract type ChannelStyle end
 struct SingleChannel <: ChannelStyle end
 struct MultiChannel <: ChannelStyle end
+ChannelStyle(::Type) = SingleChannel()
 
 abstract type EventStyle end
 struct SingleEventType <: EventStyle end
 struct MultipleEventTypes <: EventStyle end
-
-PredictorStyle(::Type) = OnlySplines()
-PredictorStyle(::Type{UnitTestSimulation}) = ManyPredictors()
-
-ChannelStyle(::Type) = SingleChannel()
-ChannelStyle(::Type{UnitTestSimulation}) = MultiChannel()
-
 EventStyle(::Type) = SingleEventType()
+
+struct BenchmarkSimulation <: SimulationType end
+
+struct UnitTestSimulation <: SimulationType end
+PredictorStyle(::Type{UnitTestSimulation}) = ManyPredictors()
+ChannelStyle(::Type{UnitTestSimulation}) = MultiChannel()
 EventStyle(::Type{UnitTestSimulation}) = MultipleEventTypes()
+
+struct AnalysisTutorialSimulation <: SimulationType end
+PredictorStyle(::Type{AnalysisTutorialSimulation}) = OnlySplines()
+ChannelStyle(::Type{AnalysisTutorialSimulation}) = MultiChannel()
+EventStyle(::Type{AnalysisTutorialSimulation}) = SingleEventType()
