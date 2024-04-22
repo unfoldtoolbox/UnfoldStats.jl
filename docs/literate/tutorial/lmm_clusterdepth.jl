d5f263639eaff6d2aed7765aaac3bce6203d8fb5
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2024-04-22 06:04:40 +0000

    inital lmm_perm

---
+++ docs/literate/tutorial/lmm_clusterdepth.jl
@@ -0,0 +1,71 @@
+
+# get some data
+using UnfoldSim
+using Unfold
+using MixedModelsPermutations, ClusterDepth # both necessary to activate correct extension!
+
+design = MultiSubjectDesign(;
+    n_subjects = 30,
+    n_items = 40,
+    items_between = Dict(:stimtype => ["car", "face"]),
+)
+#both_within = Dict(:condition=>["scrambled","intact"]))
+contrasts = Dict(:stimtype => DummyCoding())
+p1 = MixedModelComponent(;
+    basis = UnfoldSim.p100(; sfreq = srate),
+    formula = @formula(dv ~ 1 + (1 | subject) + (1 | item)),
+    β = [5.0],
+    σs = Dict(:subject => [0.0], :item => [0.0]),
+    contrasts = contrasts,
+);
+
+n1 = MixedModelComponent(;
+    basis = UnfoldSim.n170(; sfreq = srate),
+    formula = @formula(dv ~ 1 + stimtype + (1 + stimtype | subject) + (1 | item)),
+    β = [1.0, 4], # n170-basis is negative
+    σs = Dict(:subject => [2.0, 0.25], :item => [0.25]),
+    contrasts = contrasts,
+);
+
+p3 = MixedModelComponent(;
+    basis = UnfoldSim.p300(; sfreq = srate),
+    formula = @formula(dv ~ 1 + (1 | subject) + (1 + stimtype | item)),
+    β = [4.0],
+    σs = Dict(:subject => [1.0], :item => [0.5, 2]),
+    contrasts = contrasts,
+);
+
+
+
+data_e, events = UnfoldSim.simulate(
+    MersenneTwister(23),
+    design,
+    [p1, n1, p3],
+    UniformOnset(srate * 2, 10),
+    PinkNoise(; noiselevel = 1);
+    return_epoched = true,
+) # 18
+times = range(-0.1, 0.5, length = size(data_e, 1))
+
+#events.latency .+= repeat(range(0,length=size(data,2),step=size(data,1)),inner=size(events[events.subject.=="S01",:],1))
+#data_e,times = Unfold.epoch(data[:],events,[-0.1,0.5],srate)
+#events,data_e = Unfold.drop_missing_epochs(events,data_e)
+
+
+# # Fit LMM 
+m = fit(
+    UnfoldModel,
+    Dict(
+        Any => (
+            @formula(0 ~ 1 + stimtype + (1 + stimtype | item) + (1 + stimtype | subject)),
+            times,
+        ),
+    ),
+    events,
+    data_e,
+);
+
+
+# # Cluster Permute :)
+coefficient = 2
+pvalue(MersenneTwister(1), m, data_e, coefficient; n_permutations = 100)
---
+++ ext/UnfoldStatsMixedModelsPermutationsExt.jl
@@ -0,0 +1,147 @@
+module UnfoldStatsMixedModelsPermutationsExt
+using Unfold
+using UnfoldStats
+#using MixedModels
+using MixedModelsPermutations
+using ClusterDepth
+using Logging
+
+function UnfoldStat.pvalue(
+    rng,
+    model::UnfoldLinearMixedModel,
+    data,
+    coefficient;
+    type = "clusterdepth",
+    kwargs...,
+)
+    if type != "clusterdepth"
+        error("other types (e.g. FDR currently not implemented")
+    elseif type == "clusterdepth"
+        return lmm_clusterdepth(rng, model, data, coefficient; kwargs...)
+    end
+end
+
+
+
+
+function lmm_clusterdepth(
+    rng,
+    model,
+    data,
+    coefficient;
+    lmm_statistic = :z,
+    clusterforming_threshold,
+    kwargs...,
+)
+    permuted = lmm_permutations(rng, model, data, coefficient; kwargs...)
+    observed = get_lmm_statistic(model, coefficient, lmm_statistic)
+    return lmm_clusterdepth_pvalues(
+        rng,
+        observed,
+        permuted;
+        clusterforming_threshold,
+        kwargs...,
+    )
+end
+function lmm_clusterdepth_pvalues(observed, permuted; clusterforming_threshold)
+
+    # we need global variables here (yes, sorry...), because instead of actually
+    # letting ClusterDepth do the permutation, we just have to index the already
+    # permuted data given in the function (`permuted`)
+    global n_permutation_count
+    n_permutation_count = 0
+    function _fake_permutation_fun(r, data)
+        global n_permutation_count
+        n_permutation_count = n_permutation_count + 1
+        return permuted[:, n_permutation_count]
+    end
+    J_tuple = ClusterDepth.perm_clusterdepths_both(
+        MersenneTwister(1),
+        abs.(permuted),
+        _fake_permutation_fun,
+        clusterforming_threshold;
+        statfun = x -> abs.(x),
+        nₚ = size(permuted, 2),
+    )
+
+    pvals = ClusterDepth.pvals(observed, J_tuple, clusterforming_threshold)
+
+end
+
+function lmm_permutations(
+    rng::AbstractRNG,
+    model,
+    data,
+    coefficient::Int;
+    n_permutations = 500,
+    lmm_statistic = :z,
+    time_selection = 1:size(data, 2),
+)
+    permdata = Matrix{Float64}(undef, length(time_selection), n_permutations)
+    LMMext = Base.get_extension(Unfold, :UnfoldMixedModelsExt)
+    mm_outer = LMMext.LinearMixedModel_wrapper(
+        Unfold.formula(model),
+        data[1, 1, :],
+        modelmatrix(model),
+    )
+    mm_outer.optsum.maxtime = 0.1 # 
+
+    chIx = 1 # for now we only support 1 channel anyway
+    #
+    #p = Progress(length(time_selection))
+    #Threads.@threads for tIx =1:length(time_selection)
+    #@showprogress "Processing Timepoints" for 
+    Threads.@threads for tIx = 1:length(time_selection)
+
+        # splice in the correct dataa for residual calculation
+        mm = deepcopy(mm_outer)
+        mm.y .= data[chIx, time_selection[tIx], :]
+
+        # set the previous calculated model-fit
+        updateL!(MixedModels.setθ!(mm, Vector(model.modelfit.θ[time_selection[tIx]])))
+
+        # get the coefficient 
+        H0 = coef(mm)
+        # set the one of interest to 0
+        H0[coefficient] = 0
+        # run the permutation
+
+        perm = undef
+        Logging.with_logger(NullLogger()) do   # remove NLopt warnings  
+            perm = permutation(
+                deepcopy(rng), # important here is to set the same seed to keep flip all time-points the same
+                n_permutations,
+                mm;
+                β = H0,
+                progress = false,
+                #blup_method = MixedModelsPermutations.olsranef,
+            ) # constant rng to keep autocorr & olsranef for singular models
+        end
+
+        # extract the test-statistic
+
+        permdata[tIx, :] = get_lmm_statistic(permutations, coefficient, lmm_statistic)
+
+        #next!(p)
+    end # end for
+    return permdata
+end
+
+function get_lmm_statistic(
+    permutations::MixedModelFitCollection,
+    coefficient::Int,
+    lmm_statistic,
+)
+    [
+        getproperty(m, lmm_statistic) for
+        m in permutations.coefpvalues if String(m.coefname) == coefnames(mm)[coefficient]
+    ]
+
+end
+function get_lmm_statistic(model::UnfoldLinearMixedModel, coefficient::Int, lmm_statistic)
+    return get_lmm_statistic(modelfit(model).collection, coefficient, lmm_statistic)
+    #    r = coeftable(m)
+    #    r = subset(r, :group => (x -> isnothing.(x)), :coefname => (x -> x .!== "(Intercept)"))
+    #    tvals = abs.(r.estimate ./ r.stderror)
+    #    return tvals
+end
